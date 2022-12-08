0dbfd3b966893cacb808f21822461e9748fa7648
Amir9eng <amirmukhtarme@gmail.com>
2022-12-08 17:34:56 +0100

    products, footer, navbar and home page

---
+++ pages/product/[slug].js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { client, urlFor } from '../../lib/client'
+
+const ProductDetails = ({ product, products }) => {
+  return (
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src='' alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetails
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`
+  const productsQuery = '*[_type == "product"]'
+  const product = await client.fetch(query)
+  const products = await client.fetch(productsQuery)
+
+  return {
+    props: { product, products }
+  }
+}

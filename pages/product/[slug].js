e94303453ded1a18ae1b73382c222bc2875e9a6a
Amir9eng <amirmukhtarme@gmail.com>
2022-12-09 15:22:38 +0100

    slug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,37 @@
 import React from 'react'
 import { client, urlFor } from '../../lib/client'
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar
+} from 'react-icons/ai'
 
 const ProductDetails = ({ product, products }) => {
+  const { image, name, price, details } = product
   return (
     <div>
       <div className='product-detail-container'>
         <div>
           <div className='image-container'>
-            <img src='' alt='' />
+            <img src={urlFor(image && image[0])} alt='' />
+          </div>
+          {/* <div className='small-images-container'>
+            {image?.map((item, i) => (
+              <img src={urlFor(item)} className='' onMouseEnter={``} />
+            ))}
+          </div> */}
+        </div>
+        <div className='products-details-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
           </div>
         </div>
       </div>
@@ -17,11 +41,34 @@ const ProductDetails = ({ product, products }) => {
 
 export default ProductDetails
 
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }
+  `
+  const products = await client.fetch(query)
+
+  const paths = products.map(product => ({
+    params: {
+      slug: product.slug.current
+    }
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`
   const productsQuery = '*[_type == "product"]'
   const product = await client.fetch(query)
   const products = await client.fetch(productsQuery)
+
+  console.log(product)
 
   return {
     props: { product, products }

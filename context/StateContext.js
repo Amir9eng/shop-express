faafc33ff7b8f57c68acb4740ef359a32fbd2833
Amir9eng <amirmukhtarme@gmail.com>
2023-01-06 22:42:13 +0100

    increase & decrease qty

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,11 +10,32 @@ export const StateContext = ({ children }) => {
   const [totalQuantity, setTotalQuantity] = useState()
   const [qty, setQty] = useState(1)
 
+  const incQty = () => {
+    setQty(prevQty => prevQty + 1)
+  }
+
+  const decQty = () => {
+    setQty(prevQty => {
+      if (prevQty - 1 < 1) return 1
+      return prevQty - 1
+    })
+  }
+
   return (
     <Context.Provider
-      value={{ showCart, cartItems, totalPrice, totalQuantity, qty }}
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantity,
+        qty,
+        incQty,
+        decQty
+      }}
     >
-      {children}
+      {children}{' '}
     </Context.Provider>
   )
 }
+
+export const useStateContext = () => useContext(Context)
